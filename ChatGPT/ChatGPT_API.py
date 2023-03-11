25dc40046952296509f3f2bdfbc6dccfcda31f84
pecu <pecu610@gmail.com>
2023-03-11 13:54:57 +0800

    chatgpt

---
+++ ChatGPT/ChatGPT_API.py
@@ -0,0 +1,35 @@
+import openai
+
+with open('./data/程式語言_Week3_Python基礎02.txt', 'r', encoding='utf-8') as fh:
+    tmp = fh.read()
+    itemlist = tmp.split(',')
+
+
+itemlist = str(itemlist)
+
+keyfile = open("key.txt", "r")
+key = keyfile.readline()
+
+openai.api_key = key
+
+start_idx = 0
+with open('output.txt', 'w', encoding='utf-8') as output_file:
+    while start_idx < len(itemlist):
+        end_idx = min(start_idx + 1600, len(itemlist))
+        sub_list = itemlist[start_idx:end_idx]
+        
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a chatbot"},
+                {"role": "user", "content": f"提供以上文字之繁體中文摘要與英文翻譯：{sub_list}"}
+            ]
+        )
+        
+        result = ''
+        for choice in response.choices:
+            result += choice.message.content
+
+        output_file.write(result)
+
+        start_idx = end_idx
